a8f11c9e616e99b52d98ede73eaafdb2fd9c692b
JiaminShi1226 <shijiamin0324@gmail.com>
2023-06-14 20:21:37 -0400

    addRows added

---
+++ script.js
@@ -0,0 +1,15 @@
+let grid = document.getElementById("grid");
+let rows = 1;
+let columns = 1;
+
+function addRows() {
+  let row = document.createElement("tr");
+  row.classList.add("row");
+  grid.appendChild(row);
+  for (let i = 0; i < columns; i++) {
+    let column = document.createElement("td");
+    column.classList.add("column");
+    row.appendChild(column);
+  }
+  rows++;
+}
