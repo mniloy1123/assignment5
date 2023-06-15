b56a1b0b53cfad1afeb16cff8469ad72da7469a0
9Mohamedismail <87681402+9Mohamedismail@users.noreply.github.com>
2023-06-14 20:54:02 -0400

    Revert "added the addColumn function"
    This reverts commit 2d910fd821c9704d75ae970c534740db09832015.

--- script.js
+++
@@ -1,8 +0,0 @@
-const addColumn = document.getElementById("addColumns");
-addColumn.onclick = () => {
-    let trs = document.querySelectorAll("table tr")
-    for (let tr of trs) {
-        let td = document.createElement('td');
-        tr.appendChild(td);
-    }
-}
